33f1effbaad24287991fd04f31174836d51f30e9
Josh Klein <kleinjjosh74@gmail.com>
2022-05-05 22:13:49 -0400

    added post route

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -1,12 +1,30 @@
-
+const fs = require('fs');
+const path = require('path');
 const router = require('express').Router();
 const { notes } = require('../../db/db.json');
+
+function createNote(body, notesArray) {
+    const note = body;
+    notesArray.push(note);
+    fs.writeFileSync(
+        path.join(__dirname, '../../db/db.json'),
+        JSON.stringify({ notesArray }, null, 2)
+    );
+    return note;
+};
 
 router.get('/notes', (req, res) => {
     let results = notes;
 
     res.json(results);
-})
+});
+
+router.post('/notes', (req, res) => {
+    req.body.id = notes.length.toString();
+
+    const note = createNote(req.body, notes);
+    res.json(note);
+});
 
 
 
